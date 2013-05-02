5740a954b5d05602c2bc088a1549692dd543c980
Michael Borde <michael.borde@gmail.com>
2013-05-02 13:29:41 +0200

    refacto javadoc

--- src/main/java/fr/arpinum/cocoritest/Affirmations.java
+++ src/main/java/fr/arpinum/cocoritest/Affirmations.java
@@ -15,10 +15,20 @@
 
 package fr.arpinum.cocoritest;
 
-import fr.arpinum.cocoritest.affirmation.FabriqueAffirmation;
+import java.util.Collection;
+
+import fr.arpinum.cocoritest.affirmation.booleene.AffirmationBooleene;
+import fr.arpinum.cocoritest.affirmation.booleene.AffirmationBooleeneAuFeminin;
+import fr.arpinum.cocoritest.affirmation.booleene.AffirmationBooleeneDeBase;
+import fr.arpinum.cocoritest.affirmation.collection.AffirmationCollection;
+import fr.arpinum.cocoritest.affirmation.collection.AffirmationCollectionDeBase;
+import fr.arpinum.cocoritest.affirmation.objet.AffirmationObjet;
+import fr.arpinum.cocoritest.affirmation.objet.AffirmationObjetAuFeminin;
+import fr.arpinum.cocoritest.affirmation.objet.AffirmationObjetDeBase;
+import fr.arpinum.cocoritest.extensionlangage.Listes;
 
 /**
- * Le point d'entrée qui permet d'avoir accès à la fabrique d'affirmations.
+ * Le point d'entrée qui permet de créer les différentes affirmations.
  * <p>
  * Exemples :
  * <pre>
@@ -28,9 +38,173 @@ import fr.arpinum.cocoritest.affirmation.FabriqueAffirmation;
  * 	</pre>
  * </p>
  */
-public abstract class Affirmations {
+public class Affirmations {
 
-	public static FabriqueAffirmation alors() {
-		return new FabriqueAffirmation();
+	/**
+	 * Crée une fabrique d'affirmations pour différents types d'objets.
+	 *
+	 * @return la fabrique d'affirmation
+	 */
+	public static Affirmations alors() {
+		return new Affirmations();
+	}
+
+	private Affirmations() {
+	}
+
+	/**
+	 * Crée une affirmation concernant un booléen.
+	 *
+	 * @param booléen la cible de l'affirmation.
+	 * @return l'affirmation créée.
+	 */
+	public AffirmationBooleene le(boolean booléen) {
+		return new AffirmationBooleeneDeBase(booléen);
+	}
+
+	/**
+	 * Crée une affirmation concernant un booléen.
+	 * <p>
+	 * A utiliser quand le mot commence par une voyelle :
+	 * <pre>
+	 * 		alors().cet(objet).estVrai();
+	 *  </pre>
+	 * </p>
+	 *
+	 * @param booléen la cible de l'affirmation.
+	 * @return l'affirmation créée.
+	 */
+	public AffirmationBooleene cet(boolean booléen) {
+		return le(booléen);
+	}
+
+	/**
+	 * Crée une affirmation concernant un booléen.
+	 * <p>
+	 * A utiliser pour une instruction entière :
+	 * <pre>
+	 * 		alors().ceci(maVariable.estSuper()).estVrai();
+	 *  </pre>
+	 * </p>
+	 *
+	 * @param booléen la cible de l'affirmation.
+	 * @return l'affirmation créée.
+	 */
+	public AffirmationBooleene ceci(boolean booléen) {
+		return le(booléen);
+	}
+
+	/**
+	 * Crée une affirmation concernant un booléen. L'affirmation est au féminin.
+	 *
+	 * @param booléen la cible de l'affirmation.
+	 * @return l'affirmation créée.
+	 */
+	public AffirmationBooleeneAuFeminin la(boolean booléen) {
+		return new AffirmationBooleeneDeBase(booléen);
+	}
+
+	/**
+	 * Crée une affirmation concernant un booléen. L'affirmation est au féminin.
+	 * <p>
+	 * A utiliser quand le mot commence par une voyelle :
+	 * <pre>
+	 * 		alors().cette(histoire).estVraie();
+	 *  </pre>
+	 * </p>
+	 *
+	 * @param booléen la cible de l'affirmation.
+	 * @return l'affirmation créée.
+	 */
+	public AffirmationBooleeneAuFeminin cette(boolean booléen) {
+		return la(booléen);
+	}
+
+	/**
+	 * Crée une affirmation concernant un objet.
+	 *
+	 * @param objet la cible de l'affirmation.
+	 * @return l'affirmation créée.
+	 */
+	public <T> AffirmationObjet<T> le(T objet) {
+		return new AffirmationObjetDeBase<T>(objet);
+	}
+
+	/**
+	 * Crée une affirmation concernant un objet.
+	 * <p>
+	 * A utiliser quand le mot commence par une voyelle :
+	 * <pre>
+	 * 		alors().cet(objet).est(autreObjet);
+	 *  </pre>
+	 * </p>
+	 *
+	 * @param objet la cible de l'affirmation.
+	 * @return l'affirmation créée.
+	 */
+	public <T> AffirmationObjet<T> cet(T objet) {
+		return le(objet);
+	}
+
+	/**
+	 * Crée une affirmation concernant un objet.
+	 * <p>
+	 * A utiliser pour une instruction entière :
+	 * <pre>
+	 * 		alors().ceci(maVariable.getPropriété()).est(objet);
+	 *  </pre>
+	 * </p>
+	 *
+	 * @param objet la cible de l'affirmation.
+	 * @return l'affirmation créée.
+	 */
+	public <T> AffirmationObjet<T> ceci(T objet) {
+		return le(objet);
+	}
+
+	/**
+	 * Crée une affirmation concernant un objet. L'affirmation est au féminin.
+	 *
+	 * @param objet la cible de l'affirmation.
+	 * @return l'affirmation créée.
+	 */
+	public <T> AffirmationObjetAuFeminin<T> la(T objet) {
+		return new AffirmationObjetDeBase<T>(objet);
+	}
+
+	/**
+	 * Crée une affirmation concernant un objet. L'affirmation est au féminin.
+	 * <p>
+	 * A utiliser quand le mot commence par une voyelle :
+	 * <pre>
+	 * 		alors().cette(annonce).est("annonce");
+	 *  </pre>
+	 * </p>
+	 *
+	 * @param objet la cible de l'affirmation.
+	 * @return l'affirmation créée.
+	 */
+	public <T> AffirmationObjetAuFeminin<T> cette(T objet) {
+		return la(objet);
+	}
+
+	/**
+	 * Crée une affirmation concernant une collection.
+	 *
+	 * @param éléments les cibles de l'affirmation.
+	 * @return l'affirmation créée.
+	 */
+	public <T> AffirmationCollection<T> les(Collection<T> éléments) {
+		return new AffirmationCollectionDeBase<T>(éléments);
+	}
+
+	/**
+	 * Crée une affirmation concernant une collection.
+	 *
+	 * @param éléments les cibles de l'affirmation.
+	 * @return l'affirmation créée.
+	 */
+	public <T> AffirmationCollection<T> les(T[] éléments) {
+		return les(Listes.cree(éléments));
 	}
 }
